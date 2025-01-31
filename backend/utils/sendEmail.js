12b7a2eb5f1b9d14a74cd816b60dae52ccd3f2ca
dev_Angus <112677587+Angus109@users.noreply.github.com>
2025-01-31 12:23:48 +0100

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -7,15 +7,14 @@ export const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
     host: process.env.SMT_HOST,
     port: process.env.SMT_PORT,
-    service: process.env.SMT_SERVICE,
     auth: {
-      user: process.env.SERVICE,
+      user: process.env.SMT_USER,
       pass: process.env.SMT_PASSWORD,
     },
   });
 
   const mailOptions = {
-    from: "",
+    from: process.env.SMT_USER,
     to: options.email,
     subject: options.subject,
     text: options.message,
